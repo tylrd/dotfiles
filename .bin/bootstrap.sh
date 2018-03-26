5a372db943430ed6ae4966a0add14f0872a01e9f
Taylor Daugherty <taylor.daugherty@bettercloud.com>
2018-03-26 13:12:09 -0400

    json; bootstrap update

--- .bin/bootstrap.sh
+++ .bin/bootstrap.sh
@@ -1,13 +1,23 @@
 #!/usr/bin/env bash
 
+separator() {
+  echo ""
+  echo "==============================="
+  echo ""
+}
+
+echo "Install vim plugins..."
 vim +'PlugInstall --sync' +qall &> /dev/null
+echo "Done."
+separator
 
 if ! command -v brew >/dev/null; then
   echo "Installing Homebrew ..."
   curl -fsS 'https://raw.githubusercontent.com/Homebrew/install/master/install' | ruby
+  separator
 fi
 
 if [[ -f "$HOME/.Brewfile" ]]; then
-    echo "\nInstalling system dependencies from $HOME/.Brewfile"
-    brew bundle --global
+  echo "Installing system dependencies from $HOME/.Brewfile"
+  brew bundle --global
 fi
