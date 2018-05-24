a9c92290d6aa4e7dd65dfcd179d83d414628d25e
Taylor Daugherty <taylor.daugherty@bettercloud.com>
2018-05-23 23:34:14 -0400

    make directory before moving

--- .bin/install.sh
+++ .bin/install.sh
@@ -13,7 +13,7 @@ if [ $? = 0 ]; then
   echo "Checked out config.";
   else
     echo "Backing up pre-existing dot files.";
-    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mkdir -p $(dirname .config-backup/{}); mv {} .config-backup/{}
 fi;
 
 config checkout
